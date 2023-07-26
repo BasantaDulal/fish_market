635b895f7e18dd354b7929e11be0983adc0fb50f
BasantaDulal <basantdulal@gmail.com>
2023-07-25 20:01:04 -0400

    design and other update

--- app.py
+++ app.py
@@ -1,32 +1,45 @@
 from flask import Flask, render_template, request
-import numpy as np
 import joblib
 
 app = Flask(__name__)
 
-# Load the trained model from model.pkl file
+# Load the model and label encoder
 rf_classifier = joblib.load('model.pkl')
-
-# Load the label encoder from label_encoder.pkl file
 label_encoder = joblib.load('label_encoder.pkl')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # Get the input data from the form
+        Weight = float(request.form['Weight'])
+        Length1 = float(request.form['Length1'])
+        Length2 = float(request.form['Length2'])
+        Length3 = float(request.form['Length3'])
+        Height = float(request.form['Height'])
+        Width = float(request.form['Width'])
+
+        # Make a prediction using the model
+        prediction = rf_classifier.predict(
+            [[Weight, Length1, Length2, Length3, Height, Width]])
+        predicted_species = label_encoder.inverse_transform(prediction)[0]
+
+        # Prepare the input data dictionary to pass to the template
+        input_data = {
+            'Weight': Weight,
+            'Length1': Length1,
+            'Length2': Length2,
+            'Length3': Length3,
+            'Height': Height,
+            'Width': Width
+        }
+
+        # Render the template with the prediction result and input data
+        return render_template('index.html', prediction_text=predicted_species, input_data=input_data)
+
+    # Render the initial form without any prediction
+    return render_template('index.html', prediction_text=None)
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    features = [float(x) for x in request.form.values()]
-    features = np.array(features).reshape(1, -1)
-
-    # Make the prediction
-    prediction_index = rf_classifier.predict(features)[0]
-    predicted_species = label_encoder.inverse_transform([prediction_index])[0]
-
-    return render_template('index.html', prediction_text='Predicted Fish Species: {}'.format(predicted_species))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
